6ecd17c170e35b20b5f1380f31c7f5069ec26eae
am_6733 <anton6733@gmail.com>
2022-10-27 00:56:27 +0300

    default bullets

--- c_sharp/t1-3.cs
+++ c_sharp/t1-3.cs
@@ -8,11 +8,12 @@ class two_dimensional_array
     const int border_lower = -1000, border_upper = 1000;
     public static void todo(int task)
     {
+        WriteLine();
         switch (task)
         {
             case 1:
                 {
-                    WriteLine("Задайте двумерный массив размером m×n, заполненный случайными вещественными числами");
+                    WriteLine("Задайте двумерный массив размером m*n, заполненный случайными вещественными числами");
                     break;
                 }
             case 2:
@@ -116,6 +117,53 @@ class two_dimensional_array
         WriteLine(strikethrough);
         return null!;
     }
+
+    public dynamic summarize_lines(double[,] array_2d_i)
+    {
+        //ахаха :(
+        string sup_val_line;
+        double sum_buffer;
+        string left_border = "| ", right_border = " |", separator = " | ",implication=" ==> ";
+        string[] sup_arr_line;
+        int max_expression_length = 0;
+        string expression_stringified = String.Empty;
+        for (int horizontal_step = 0; horizontal_step < array_2d_i.GetLength(0); horizontal_step++)
+        {
+            for (int vertical_step = 0; vertical_step < array_2d_i.GetLength(1); vertical_step++)
+            {
+                expression_stringified = array_2d_i[horizontal_step, vertical_step].ToString();
+                max_expression_length =
+                expression_stringified.Length > max_expression_length ?
+                expression_stringified.Length : max_expression_length;
+            }
+        }
+        max_expression_length += 2;
+        string strikethrough = new string('-',
+        max_expression_length * (array_2d_i.GetLength(0)) +
+        left_border.Length +
+        right_border.Length +
+        separator.Length * (array_2d_i.GetLength(0) - 1));
+        WriteLine(strikethrough);
+        for (int horizontal_step = 0; horizontal_step < array_2d_i.GetLength(0); horizontal_step++)
+        {
+            sum_buffer=0.0;
+            sup_arr_line = new string[array_2d_i.GetLength(0)];
+            for (int vertical_step = 0; vertical_step < array_2d_i.GetLength(1); vertical_step++)
+            {
+                sum_buffer+=array_2d_i[horizontal_step, vertical_step];
+                expression_stringified = array_2d_i[horizontal_step, vertical_step].ToString();
+                sup_arr_line[vertical_step] = expression_stringified;
+            }
+            for (int normalize = 0; normalize < sup_arr_line.Length; normalize++)
+            {
+                sup_arr_line[normalize] = sup_arr_line[normalize].PadRight(max_expression_length);
+            }
+            sup_val_line = $"{left_border}{string.Join(separator, sup_arr_line)}{right_border}{implication}{sum_buffer}";
+            WriteLine(sup_val_line);
+        }
+        WriteLine(strikethrough);
+        return null!;
+    }
     public dynamic get_value_by_indexes(int position_horizontal, int position_vertical,double[,] array_2d_i)
     {
         string output=$"Размерность массива не соответсвует запросу [{position_horizontal},{position_vertical}]";
@@ -128,11 +176,15 @@ class two_dimensional_array
     }
     public dynamic main()
     {
+        Clear();
         two_dimensional_array.todo(1);
         print_2d_array(array_2d_i: this.content_random_fractured_numbers);
         two_dimensional_array.todo(2);
         WriteLine(get_value_by_indexes(position_horizontal:1,position_vertical:100, array_2d_i:this.content_random_fractured_numbers));
         WriteLine(get_value_by_indexes(position_horizontal:0,position_vertical:0, array_2d_i:this.content_random_fractured_numbers));
+        two_dimensional_array.todo(3);
+        summarize_lines(array_2d_i:this.content_random_whole_numbers);
+
         return null!;
     }
 
